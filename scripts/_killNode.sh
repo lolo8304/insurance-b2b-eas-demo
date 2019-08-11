d427ebf7b6c6693c43b2b1e2aa6b0d852c635395
Lorenz Haenggi <lolo8304@gmail.com>
2019-08-11 02:40:19 +0000

    adapt new names for sidis

--- scripts/_killNode.sh
+++ scripts/_killNode.sh
@@ -1,1 +1,1 @@
-ps -ef | grep corda | grep -v "com.template.webserver.Starter" | grep java | grep -v grep | grep -v IntelliJ | grep $1 | awk '{print "kill -9 " $2 ";" }'
+ps -ef | grep corda | grep -v ".webserver.Starter" | grep java | grep -v grep | grep -v IntelliJ | grep $1 | awk '{print "kill -9 " $2 ";" }'
--- scripts/killServers.sh
+++ scripts/killServers.sh
@@ -1,1 +1,1 @@
-ps -ef | grep "com.template.webserver.Starter" | grep java | grep -v grep | grep -v IntelliJ | awk '{print "kill -9 " $2 ";" }'
+ps -ef | grep ".webserver.Starter" | grep java | grep -v grep | grep -v IntelliJ | awk '{print "kill -9 " $2 ";" }'
