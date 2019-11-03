abb72dbe6bd2957308a6798f76f80d9e46c08a3f
Lorenz Hänggi <lolo8304@gmail.com>
2019-11-03 16:39:55 +0100

    fix all scripts

--- scripts/checkStates.sh
+++ scripts/checkStates.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-sshpass -p test ssh user1@localhost -p 10103 -oStrictHostKeyChecking=accept-new -oUserKnownHostsFile=/dev/null run vaultQuery contractStateType: com.sidis.eas.states.ServiceState
-sshpass -p test ssh user1@localhost -p 10106 -oStrictHostKeyChecking=accept-new -oUserKnownHostsFile=/dev/null run vaultQuery contractStateType: com.sidis.eas.states.ServiceState
-sshpass -p test ssh user1@localhost -p 10109 -oStrictHostKeyChecking=accept-new -oUserKnownHostsFile=/dev/null run vaultQuery contractStateType: com.sidis.eas.states.ServiceState
-sshpass -p test ssh user1@localhost -p 10112 -oStrictHostKeyChecking=accept-new -oUserKnownHostsFile=/dev/null run vaultQuery contractStateType: com.sidis.eas.states.ServiceState
-sshpass -p test ssh user1@localhost -p 10115 -oStrictHostKeyChecking=accept-new -oUserKnownHostsFile=/dev/null run vaultQuery contractStateType: com.sidis.eas.states.ServiceState
+i=10103
+nodes=( $NodeNames )
+for n in "${nodes[@]}"
+do
+  echo "check State $n"
+  sshpass -p test ssh user1@localhost -p 10115 -oStrictHostKeyChecking=accept-new -oUserKnownHostsFile=/dev/null run vaultQuery contractStateType: com.sidis.eas.states.ServiceState
+  (( i=i+3 ))
+done
--- scripts/stopNodes.sh
+++ scripts/stopNodes.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-sshpass -p test ssh user1@localhost -p 10103 -oStrictHostKeyChecking=accept-new -oUserKnownHostsFile=/dev/null run gracefulShutdown
-sshpass -p test ssh user1@localhost -p 10106 -oStrictHostKeyChecking=accept-new -oUserKnownHostsFile=/dev/null run gracefulShutdown
-sshpass -p test ssh user1@localhost -p 10109 -oStrictHostKeyChecking=accept-new -oUserKnownHostsFile=/dev/null run gracefulShutdown
-sshpass -p test ssh user1@localhost -p 10112 -oStrictHostKeyChecking=accept-new -oUserKnownHostsFile=/dev/null run gracefulShutdown
-sshpass -p test ssh user1@localhost -p 10115 -oStrictHostKeyChecking=accept-new -oUserKnownHostsFile=/dev/null run gracefulShutdown
 
+i=10103
+nodes=( $NodeNames )
+for n in "${nodes[@]}"
+do
+  echo "check State $n"
+  sshpass -p test ssh user1@localhost -p $i -oStrictHostKeyChecking=accept-new -oUserKnownHostsFile=/dev/null run gracefulShutdown
+  (( i=i+3 ))
+done
